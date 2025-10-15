30a19943d67b7f42e2e96fffc87a25aa12cc8125
duongabbcd <duong98it@gmail.com>
2025-10-15 17:55:24 +0700

    encrypt and decrypt media files p3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,12 +24,13 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+//            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
